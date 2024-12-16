37847ad005b738af30df5e09962c4fafd0295a0c
SaiSundhar173 <jss1732000@gmail.com>
2024-12-16 11:57:36 +0530

    Initial commit - Hello Username

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GreetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the username from the TextBox
+            string username = UsernameTextBox.Text;
+
+            // Check if the username is empty
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                GreetingLabel.Content = "Please enter a valid username!";
+            }
+            else
+            {
+                // Display the greeting
+                GreetingLabel.Content = $"Hello, {username}!";
+            }
+        }
+    }
+}
